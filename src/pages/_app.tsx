d1450e3acc052e90c4d14171c8d3542eab5fdb80
Fabricio Ballon <fballoncuadros@gmail.com>
2022-04-28 17:45:55 -0500

    Added react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,31 @@
-import "styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import { Layout } from "layout";
-import { CssBaseline } from "@mui/material";
-import Head from "next/head";
+import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Layout } from 'layout';
+import { CssBaseline } from '@mui/material';
+import Head from 'next/head';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/home");
+    router.push('/home');
   }, []);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <CssBaseline />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </QueryClientProvider>
   );
 }
 
